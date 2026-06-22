e778d60c4a61ba950ede806418829219a22c289b
thelilyluna <poorvi08mitra@gmail.com>
2026-06-22 17:30:59 +0530

    Refactor Prim's algorithm implementation in C

--- 14.c
+++ 14.c
@@ -1,191 +1,209 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <time.h>
 
-int heapCount = 0, graphCount = 0, n;
+#define MAX 100
 
-typedef struct pair
+int heapCount = 0, graphCount = 0;
+
+typedef struct
 {
     int node;
-    int val;
+    int cost;
     int parent;
-} *Pair;
+} Pair;
 
-Pair createPair(int n, int val, int parent)
+Pair heap[MAX * MAX];
+int heapSize;
+
+void swap(Pair *a, Pair *b)
 {
-    Pair p = (Pair)malloc(sizeof(struct pair));
-    p->val = val;
-    p->node = n;
-    p->parent = parent;
-    return p;
+    Pair temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-typedef struct PriorityQ
+void insert(Pair p)
 {
-    Pair *arr;
-    int currSize;
-} *PriorityQueue;
+    heap[heapSize] = p;
+    int i = heapSize++;
 
-PriorityQueue init(int edges)
-{
-    PriorityQueue pq = (PriorityQueue)malloc(sizeof(struct PriorityQ));
-    pq->arr = (Pair *)malloc(edges * sizeof(struct pair));
-    pq->currSize = 0;
-
-    return pq;
-}
-
-void add(PriorityQueue pq, Pair p)
-{
-    pq->arr[pq->currSize] = p;
-    pq->currSize += 1;
-
-    int x = pq->currSize - 1;
-    int parIdx = (x - 1) / 2;
-
-    while (pq->arr[x]->val < pq->arr[parIdx]->val)
+    while (i > 0)
     {
-        Pair temp = pq->arr[x];
-        pq->arr[x] = pq->arr[parIdx];
-        pq->arr[parIdx] = temp;
+        int par = (i - 1) / 2;
 
-        x = parIdx;
-        parIdx = (x - 1) / 2;
+        if (heap[par].cost <= heap[i].cost)
+            break;
+
+        swap(&heap[par], &heap[i]);
+        i = par;
     }
 }
 
-void heapify(PriorityQueue pq, int i)
+void heapify(int i)
 {
+    int smallest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
-    int minIdx = i;
+
     heapCount++;
 
-    if (left < pq->currSize && pq->arr[minIdx]->val > pq->arr[left]->val)
-    {
-        minIdx = left;
-    }
+    if (left < heapSize &&
+        heap[left].cost < heap[smallest].cost)
+        smallest = left;
 
-    if (right < pq->currSize && pq->arr[minIdx]->val > pq->arr[right]->val)
-    {
-        minIdx = right;
-    }
+    if (right < heapSize &&
+        heap[right].cost < heap[smallest].cost)
+        smallest = right;
 
-    if (minIdx != i)
+    if (smallest != i)
     {
-        Pair temp = pq->arr[i];
-        pq->arr[i] = pq->arr[minIdx];
-        pq->arr[minIdx] = temp;
-
-        heapify(pq, minIdx);
+        swap(&heap[i], &heap[smallest]);
+        heapify(smallest);
     }
 }
 
-Pair delete(PriorityQueue pq)
+Pair deleteMin()
 {
-    Pair p = pq->arr[0];
+    Pair min = heap[0];
 
-    Pair temp = p;
-    pq->arr[0] = pq->arr[pq->currSize - 1];
-    pq->arr[pq->currSize - 1] = temp;
+    heap[0] = heap[heapSize - 1];
+    heapSize--;
 
-    pq->currSize -= 1;
+    if (heapSize > 0)
+        heapify(0);
 
-    heapify(pq, 0);
-    return p;
+    return min;
 }
 
-int prims(int n, int adjMat[n][n])
+int prims(int n, int adj[n][n])
 {
-    int vis[n], finalCost = 0, MSTEdge[n][2], k = 0;
+    int vis[n];
+    int mst[n][2];
+    int k = 0;
+    int cost = 0;
+
     for (int i = 0; i < n; i++)
-    {
         vis[i] = 0;
-    }
-    int edges = 0;
 
-    for (int i = 0; i < n; i++)
+    heapSize = 0;
+
+    insert((Pair){0, 0, -1});
+
+    while (heapSize)
     {
-        for (int j = 0; j < n; j++)
+        Pair curr = deleteMin();
+
+        if (vis[curr.node])
+            continue;
+
+        vis[curr.node] = 1;
+
+        mst[k][0] = curr.parent;
+        mst[k][1] = curr.node;
+        k++;
+
+        cost += curr.cost;
+
+        for (int i = 0; i < n; i++)
         {
-            if (i != j && adjMat[i][j] != INT_MAX)
-                edges++;
-        }
-    }
-
-    PriorityQueue pq = init(edges);
-    add(pq, createPair(0, 0, -1));
-
-    while (pq->currSize != 0)
-    {
-        Pair curr = delete (pq);
-        if (!vis[curr->node])
-        {
-            vis[curr->node] = 1;
-            MSTEdge[k][0] = curr->parent;
-            MSTEdge[k++][1] = curr->node;
-            finalCost += curr->val;
-
             graphCount++;
 
-            for (int i = 0; i < n; i++)
+            if (!vis[i] &&
+                adj[curr.node][i] != INT_MAX &&
+                curr.node != i)
             {
-                if (curr->node != i && adjMat[curr->node][i] != INT_MAX)
-                {
-                    graphCount++;
-                    add(pq, createPair(i, adjMat[curr->node][i], curr->node));
-                }
+                insert((Pair)
+                           {i,
+                            adj[curr.node][i],
+                            curr.node});
             }
         }
     }
 
-    printf("MST edges are :\n");
-    for (int i = 1; i < n; i++)
-    {
-        printf("%d -- %d\n", MSTEdge[i][0], MSTEdge[i][1]);
-    }
+    printf("\nMST Edges:\n");
+    for (int i = 1; i < k; i++)
+        printf("%d -- %d\n", mst[i][0], mst[i][1]);
 
-    return finalCost;
+    return cost;
 }
 
 void tester()
 {
-    int source;
-    printf("Enter the number of nodes : ");
+    int n;
+
+    printf("Enter number of vertices : ");
     scanf("%d", &n);
-    printf("Enter adjacency matrix :\n");
-    int adjMat[n][n];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &adjMat[i][j]);
+
+    int adj[n][n];
+
+    printf("Enter adjacency matrix (-1 for no edge):\n");
 
     for (int i = 0; i < n; i++)
+    {
         for (int j = 0; j < n; j++)
-            if (adjMat[i][j] == -1)
-                adjMat[i][j] = INT_MAX;
+        {
+            scanf("%d", &adj[i][j]);
 
-    printf("\nMinimum cost of MST : %d\n", prims(n, adjMat));
-    printf("\nheapCount : %d\tGraphCount: %d\n", heapCount, graphCount);
+            if (adj[i][j] == -1)
+                adj[i][j] = INT_MAX;
+        }
+    }
+
+    heapCount = graphCount = 0;
+
+    int cost = prims(n, adj);
+
+    printf("\nMinimum Cost = %d\n", cost);
+    printf("Heap Count = %d\n", heapCount);
+    printf("Graph Count = %d\n", graphCount);
 }
 
 void plotter()
 {
-    FILE *f = fopen("prims.txt", "w");
-    for (int i = 0; i < 5; i++)
+    FILE *fp = fopen("prims.txt", "w");
+
+    srand(time(NULL));
+
+    for (int n = 10; n <= 100; n += 10)
     {
-        heapCount = 0;
-        graphCount = 0;
-        tester();
-        fprintf(f, "%d\t%d\n", n, graphCount > heapCount ? graphCount : heapCount);
+        int adj[n][n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == j)
+                    adj[i][j] = 0;
+                else
+                {
+                    int wt = rand() % 100 + 1;
+                    adj[i][j] = wt;
+                    adj[j][i] = wt;
+                }
+            }
+        }
+
+        heapCount = graphCount = 0;
+
+        prims(n, adj);
+
+        fprintf(fp, "%d\t%d\n",
+                n,
+                heapCount + graphCount);
     }
 
-    fclose(f);
+    fclose(fp);
+
+    printf("\nData stored in prims.txt\n");
 }
 
-void main()
+int main()
 {
     int ch;
-    printf("\n\nEnter\n1.Tester\n2.Plotter\n");
+
+    printf("\n1. Tester\n2. Plotter\nChoice : ");
     scanf("%d", &ch);
 
     switch (ch)
@@ -193,10 +211,14 @@ void main()
     case 1:
         tester();
         break;
+
     case 2:
         plotter();
         break;
+
     default:
-        printf("Invalid Input!");
+        printf("Invalid Choice\n");
     }
+
+    return 0;
 }
